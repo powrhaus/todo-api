45190ffafbdf5ce01c0a530f177c620f0f8a86e4
Patrick Power <powrhaus2001@gmail.com>
2019-08-22 15:48:43 -0500

    add /todos and /todos/:id endpoints

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3111;
+var todos = [{
+    id: 1,
+    description: 'Go to Movie',
+    completed: false
+}, {
+    id: 2,
+    description: 'Get a job',
+    completed: false
+},{
+    id: 3,
+    description: 'Watch Cowboys pre-season game 2',
+    completed: true
+}];
 
 app.get('/', function(req,res){
     res.send('ToDo API root');
+});
+
+//  GET /todos  - get all TODOs
+app.get('/todos', function(req,res){
+    res.json(todos);
+});
+
+//  GET /todos/:id  - a single TODO
+app.get('/todos/:id', function(req,res){
+    var todoID = parseInt(req.params.id,10);
+    todos.forEach(item =>{
+        if (item.id === todoID) {
+            res.json(item);
+        }
+    });
+    res.status(404).send();
 });
 
 app.listen(PORT, function(){
