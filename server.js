ff0615e53c70958d70c8b197839ed1c97025a8ff
Patrick Power <powrhaus2001@gmail.com>
2019-08-23 14:51:14 -0500

    add PUT method

--- server.js
+++ server.js
@@ -61,6 +61,38 @@ app.delete('/todos/:id', function(req,res){
 
 });
 
+//  PUT /todos/:id  - update a single TODO
+app.put('/todos/:id', function(req,res){
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    var todoID = parseInt(req.params.id,10);
+    var matchedTodo = _.findWhere(todos,{id: todoID});
+
+    if (!matchedTodo){
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    } else {
+        // completed was not provided, so we are not updating it
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description.trim();
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    } else {
+        // description was not provided, so we are not updating it
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+
+});
+
 app.listen(PORT, function(){
     console.log('Server listening on port: '+ PORT);
 });
